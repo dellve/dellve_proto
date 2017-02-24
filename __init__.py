851c797e289f0938eb25cf145bf69d4737768e1a
Konstantyn Komarov <komarov.konstant@utexas.edu>
2017-02-24 17:36:38 -0600

    Modified benchend API

---
+++ __init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    'types',
+    'backend',
+    'backend_grpc',
+    'benchend',
+    'benchend_grpc'
+]
+
+import dellve_types_pb2         as types
+import dellve_backend_pb2       as backend
+import dellve_backend_pb2_grpc  as backend_grpc
+import dellve_benchend_pb2      as benchend
+import dellve_benchend_pb2_grpc as benchend_grpc
